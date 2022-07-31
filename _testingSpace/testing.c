2f1ecaaa6e1b0d21fbe0edc12e323fb30a3a6214
Ethan <ethangbraun@gmail.com>
2022-07-31 00:47:59 -0500

    Working on more file io

--- _testingSpace/testing.c
+++ _testingSpace/testing.c
@@ -1,31 +1,16 @@
-#define _GNU_SOURCE
+#define _FILE_OFFSET_BITS 64
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <limits.h>
-#include <sys/uio.h>
+#include "../lib/tlpi_hdr.h"
 
 int main(int argc, char *argv[]) {
-    int length1 = 64;
-    int length2 = 128;
-    int length3 = 16;
-
-    char buf1[length1];
-    char buf2[length2];
-    char buf3[length3];
-
-    struct iovec iostruct[] = {
-        {buf1, length1},
-        {buf2, length2},
-        {buf3, length3}
-    };
-
-    int fd = open("myfile.txt", O_RDONLY);
-    int wfd = open("output.txt", O_WRONLY | O_TRUNC);
-    readv(fd, iostruct, 3);
-    writev(wfd, iostruct, 3);
+    int fd = open("myfile.txt", O_APPEND | O_RDWR);
+    if (fd == -1)
+        perror("open");
     
-    return 0;
+    lseek(fd, 0, SEEK_SET);
+
+    char* buf = "test";
+
+    ssize_t bytesWritten = write(fd, buf, 4);
 }
